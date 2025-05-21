99de6904a5a9367e9678c2d3ebf336e8e5e79dbf
Ryo Kanbayashi <ryo.contact@gmail.com>
2025-05-22 02:11:44 +0900

    change frontend access backend port 3000 to 10000.

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
     if (isPlaying && images.length > 0) {
       const interval = window.setInterval(() => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 443); //3000);
+      }, 10000); //443); //3000);
       setSlideshowInterval(interval);
     } else if (slideshowInterval !== null) {
       clearInterval(slideshowInterval);
